0736b188d064dfdf92496f3970d5611d9f1cad5e
wbscj <wmy_weifeng@163.com>
2022-04-07 14:54:30 +0800

    第四次推送

--- src/main/java/com/example/singup/SingupApplication.java
+++ src/main/java/com/example/singup/SingupApplication.java
@@ -2,9 +2,19 @@ package com.example.singup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SingupApplication {
+public class SingupApplication extends SpringBootServletInitializer {
+
+
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(SingupApplication.class);
+        }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(SingupApplication.class, args);
